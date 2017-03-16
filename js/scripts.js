3a68cdf62fa5c33d249e310c061844ae2086c426
justdealman <justdealman@gmail.com>
2017-03-16 16:07:36 +0700

    card hover update

--- js/scripts.js
+++ js/scripts.js
@@ -129,7 +129,7 @@ $(function() {
 	$('.catalog-item img, .portfolio__list .pic').tilt({
 		maxTilt: 3
 	});
-	$('.item-category .pic, .section .item-new, [data-scrollable="3"] .portfolio__list .pic').tilt({
+	$('.item-category .pic, [data-scrollable="3"] .portfolio__list .pic').tilt({
 		maxTilt: 4
 	});
 	$('.fullpage').fullpage({
@@ -194,60 +194,4 @@ $(function() {
 			});
 		}, 400);
 	});
-	
-	/*
-	if ( $('.slider').length > 0 ) {
-		slider();
-		$('.slider').bind('swipeleft', function() {
-			$('.slider .next').trigger('click');
-		});
-		$('.slider').bind('swiperight', function() {
-			$('.slider .prev').trigger('click');
-		});
-	}
-	if ( $('.card').length > 0 ) {
-		$('.card .photos').slides({
-			generatePagination: true,
-			generateNextPrev: true,
-			container: 'container',
-			effect: 'slide',
-			slideSpeed: 500,
-			slideEasing: 'easeInOutQuad',
-			play: 10000,
-			pause: 2500,
-		});
-		$('.card .photos').bind('swipeleft', function() {
-			$('.card .photos .next').trigger('click');
-		});
-		$('.card .photos').bind('swiperight', function() {
-			$('.card .photos .prev').trigger('click');
-		});
-	}
-	if ( $('.similar').length > 0 ) {
-		$('.similar ul').jcarousel({
-			scroll: 5,
-			animation: 500,
-			easing: 'easeInOutQuad',
-			wrap: 'circular'
-		});
-		$('.similar .jcarousel-container').bind('swipeleft', function() {
-			$('.similar .jcarousel-container .jcarousel-next').trigger('click');
-		});
-		$('.similar .jcarousel-container').bind('swiperight', function() {
-			$('.similar .jcarousel-container .jcarousel-prev').trigger('click');
-		});
-	}
-	$('input, textarea').each(function() {
-		$(this).data('holder', $(this).attr('placeholder'));
-		$(this).focusin(function() {
-			$(this).attr('placeholder', '');
-		});
-		$(this).focusout(function() {
-			$(this).attr('placeholder', $(this).data('holder'));
-		});
-	});
-	var c = 0;
-	$('header').load(function() {
-		console.log('asdasd');
-	});*/
 });
